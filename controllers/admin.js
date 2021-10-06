6807f59488a6eb6d0fe1d3e0b047cbb3dda9a451
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-10-05 23:24:23 -0500

    New updates!

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,57 +18,71 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const author = req.body.author;
   const pyear = req.body.pyear;
-  const product = new Product(null, title, isbn, price, description, author, pyear);
-  product.save();
-  res.redirect('/');
-};
-
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect('/');
-  }
-  const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      Navpath: '/admin/edit-product',
-      SiteName: GeneralAppName,
-      editing: editMode,
-      product: product
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      isbn: isbn,
+      author: author,
+      pyear: pyear,
+      description: description
+    })
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
 };
 
-exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const title = req.body.title;
-  const isbn = req.body.isbn;
-  const price = req.body.price;
-  const description = req.body.description;
-  const author = req.body.author;
-  const pyear = req.body.pyear;
-  const product = new Product(prodId, title, isbn, price, description, author, pyear);
-  product.save();
-  res.redirect('/admin/products');
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect('/');
+//   }
+//   const prodId = req.params.productId;
+//   Product.findById(prodId, product => {
+//     if (!product) {
+//       return res.redirect('/');
+//     }
+//     res.render('admin/edit-product', {
+//       pageTitle: 'Edit Product',
+//       Navpath: '/admin/edit-product',
+//       SiteName: GeneralAppName,
+//       editing: editMode,
+//       product: product
+//     });
+//   });
+// };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      SiteName: GeneralAppName,
-      pageTitle: 'Admin Products',
-      Navpath: 'AdminProducts'
-    });
-  });
-};
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const title = req.body.title;
+//   const isbn = req.body.isbn;
+//   const price = req.body.price;
+//   const description = req.body.description;
+//   const author = req.body.author;
+//   const pyear = req.body.pyear;
+//   const product = new Product(prodId, title, isbn, price, description, author, pyear);
+//   product.save();
+//   res.redirect('/admin/products');
+// };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
-};
+// exports.getProducts = (req, res, next) => {
+//   Product.fetchAll(products => {
+//     res.render('admin/products', {
+//       prods: products,
+//       SiteName: GeneralAppName,
+//       pageTitle: 'Admin Products',
+//       Navpath: 'AdminProducts'
+//     });
+//   });
+// };
+
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.deleteById(prodId);
+//   res.redirect('/admin/products');
+// };
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -28,7 +28,6 @@ app.use(errorController.get404);
 
 //app.listen(3000);
 
-mongoConnect((client) =>{
-    console.log(client);
+mongoConnect(() =>{
     app.listen(3000);
 });
