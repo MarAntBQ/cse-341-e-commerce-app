f31a6e4ce803f75fab41519759acd754c3072863
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-10-21 20:04:49 -0500

    Validation proccess with emails and passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const GeneralAppName = "MABooks";
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 //const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
@@ -90,6 +91,16 @@ exports.postSignup = (req, res, next) => {
   const uName = req.body.uName;
   const email = req.body.email;
   const password = req.body.password;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      SiteName: GeneralAppName,
+      Navpath: 'Signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
   User.findOne({
       email: email
     })
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const {
+    check, body
+} = require('express-validator');
+
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -18,7 +22,33 @@ router.post('/new-password', authController.postNewPassword);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+    '/signup',
+    [
+        check('email')
+        .isEmail()
+        .withMessage('Please enter a valid Email')
+        .custom((value, {req}) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address is forbidden.');
+            }
+            return true;
+        }),
+        body(
+            'password', 
+            'Please enter a password with only numbers and text and at least 5 characters')
+        .isLength({min: 5})
+        .isAlphanumeric(),
+        body ('confirmPassword')
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+            return true;
+        })
+
+    ],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
