4b8b923c570ba2c4a128a208d7fbdbddd8ebdd6b
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-10-21 20:29:50 -0500

    Promise!

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const {
-    check, body
+    check,
+    body
 } = require('express-validator');
 
 const authController = require('../controllers/auth');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -28,19 +31,33 @@ router.post(
         check('email')
         .isEmail()
         .withMessage('Please enter a valid Email')
-        .custom((value, {req}) => {
-            if (value === 'test@test.com') {
-                throw new Error('This email address is forbidden.');
-            }
-            return true;
+        .custom((value, {
+            req
+        }) => {
+            // if (value === 'test@test.com') {
+            //     throw new Error('This email address is forbidden.');
+            // }
+            // return true;
+            return User.findOne({
+                    email: value
+                })
+                .then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject('Email exists already, please enter a different one.');
+                    }
+                })
         }),
         body(
-            'password', 
+            'password',
             'Please enter a password with only numbers and text and at least 5 characters')
-        .isLength({min: 5})
+        .isLength({
+            min: 5
+        })
         .isAlphanumeric(),
-        body ('confirmPassword')
-        .custom((value, {req}) => {
+        body('confirmPassword')
+        .custom((value, {
+            req
+        }) => {
             if (value !== req.body.password) {
                 throw new Error('Passwords have to match!');
             }
